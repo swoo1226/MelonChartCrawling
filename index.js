cc7f574fa775740b89a39e87c6c044946e48b1d2
swoo1226 <copycat1226@gmail.com>
2019-11-10 22:15:03 +0900

    멜론 top 100 차트 곡명 가수명 엑셀 파일 저장 가능

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const log = console.log;
-
+const XLSX = require('xlsx');
 const getHtml = async () => {
     try {
         return await axios.get("https://www.melon.com/chart/index.htm");
     } catch (error) {
         console.error(error);
     }
+};
+const _export = function (records, opts) {
+    let fileName = opts + '.xlsx';
+    let workSheet = XLSX.utils.json_to_sheet(records);
+    let wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, workSheet, '멜론Top100');
+
+    let bin = XLSX.writeFile(wb, fileName);
+    return bin;
 };
 
 getHtml()
@@ -19,8 +28,12 @@ getHtml()
             let title = $(this).find('td div.wrap div.wrap_song_info div.ellipsis.rank01 span a').text()
             let singer = $(this).find('td div.wrap div.wrap_song_info div.ellipsis.rank02 span a').text()
             console.log(key, title + ': ' + singer)
-            ulList[key] = {순위 : key + 1, title, singer};
+            ulList[key] = {순위 : key, title, singer};
         })
         return ulList;
     })
-    .then(res => log(res));
+    .then(res => res)
+    .then(list => _export(list, '멜론Top100'));
+
+
+    
